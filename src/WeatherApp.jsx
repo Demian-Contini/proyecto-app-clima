6b8c616ec440ed0f17ba919b046148cb1b88d5ba
Demian Contini <demiandtc@gmail.com>
2025-11-30 14:29:21 -0300

    feat:Iniciando el proyecto 'App del clima con React'

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,29 @@
+import "./WeatherApp.css";
+
+
+export const WeatherApp = () => {
+  
+  
+  const urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+  // Ponemos la URL hasta 'weatrher' lo que le sigue es todo dinamico
+  const API_KEY = '21e2816742d266b46510619a21e9f3ef' // Key
+  const diffKelvin = 273.15 // Cambio a grados Celsious
+  
+  
+    return (
+    
+    <div className = "container">
+
+    <h1>Aplicación del clima actual</h1>
+
+    <form>
+        
+        <input type="text" placeholder = "Ingresa una ciudad"/>
+        <button type = "submit"> Buscar </button>
+
+    </form>
+
+    </div>
+    
+  )
+}
